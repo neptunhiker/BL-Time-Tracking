24391e3a520309966ee822f36bff1742887c8119
Sebastian B. Schmitz <bastiusa47@yahoo.de>
2021-10-31 21:58:59 +0100

    Clean up
    Deleted files and removed weird HEAD from files as well as weird duplicate code in files

--- footer.py
+++ footer.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-
 from reportlab.platypus import Table
 from reportlab.lib import colors
 
@@ -33,40 +30,3 @@ def gen_footer_table(width, height):
 
     ])
     return res
-||||||| 674af40
-=======
-
-
-from reportlab.platypus import Table
-from reportlab.lib import colors
-
-
-def gen_footer_table(width, height):
-    text = "BeginnerLuft gGmbH - Bandelstr. 1 - 10559 Berlin - www.beginnerluft.de"
-
-    width_list = [
-        0.1 * width,
-        0.8 * width,
-        0.1 * width,
-    ]
-
-    res = Table([
-        ["", text, ""]
-        ],
-        rowHeights=height,
-        colWidths=width_list
-    )
-
-    color = "#FFFF00"
-    # color = colors.darkgrey
-    res.setStyle([
-        # ("GRID", (0, 0), (-1, -1), 1, "red"),
-        ("LEFTPADDING", (0, 0), (-1, -1), 0),
-        # ("BACKGROUND", (0, 0), (-1, -1), color),
-        ("TEXTCOLOR", (0, 0), (-1, -1), "black"),
-        ("ALIGN", (0, 0), (-1, -1), "CENTER"),  # horizontal
-        ("VALIGN", (0, 0), (-1, -1), "MIDDLE"),
-
-    ])
-    return res
->>>>>>> a9e9800e95ddb04d17fb9f16cf9433cec7ae1a39
