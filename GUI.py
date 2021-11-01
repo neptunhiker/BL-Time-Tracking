f21e66192f6c10f80a7389edc5f6306755cb9e4e
Sebastian B. Schmitz <bastiusa47@yahoo.de>
2021-11-01 22:55:06 +0100

    Added back and forward buttons
    Back and forwards buttons also include the collection of data functionality

--- GUI.py
+++ GUI.py
@@ -18,6 +18,7 @@ class BeginnerLuftGUI(tk.Tk):
                              font=("Times New Roman", 36, "bold"))
         self.style.configure("Nav.TLabel", justify="CENTER", background="lightgrey", foreground="black",
                              font=("Times New Roman", 12, "bold"))
+        self.style.configure("BackForw.TLabel", background="yellow", foreground="black", font=("Times New Roman", 10, "bold"))
         self.style.configure("Inactive.Nav.TLabel", justify="CENTER", background="lightgrey", foreground="grey",
                              font=("Times New Roman", 12))
         self.style.configure("Data.TLabel", justify="CENTER", background="yellow", foreground="black",
@@ -26,6 +27,14 @@ class BeginnerLuftGUI(tk.Tk):
                              font=("Times New Roman", 20, "bold"))
         self.style.configure("Data.TEntry", width=60)
         self.geometry("500x300")
+
+        self.participant_name = ""
+        self.training_name = "Individuelles Berufscoaching"
+        self.training_nr = "962/400/20"
+
+        self.ent_name = None
+        self.ent_t_name = None
+        self.ent_t_nr = None
 
         self.frame_intro = ttk.Frame(self, style="Intro.TFrame")
         self.active_frame = self.frame_intro
@@ -45,6 +54,8 @@ class BeginnerLuftGUI(tk.Tk):
 
     def create_menu_functions(self, event):
         self.active_frame.destroy()
+        self.nav_frame.destroy()
+
         self.active_frame = ttk.Frame(self, style="Intro.TFrame")
         self.active_frame.grid(row=1, column=0, sticky="nsew")
         self.active_frame.grid_rowconfigure(0, weight=1)
@@ -70,27 +81,95 @@ class BeginnerLuftGUI(tk.Tk):
 
         lbl_name = ttk.Label(self.active_frame, text="Teilnehmer:in", style="Data.TLabel")
         lbl_name.grid(row=0, column=0, sticky="se")
-        ent_name = ttk.Entry(self.active_frame, style="Data.TEntry", width=30)
-        ent_name.grid(row=0, column=1, sticky="sw", padx=(20,0))
+        self.ent_name = ttk.Entry(self.active_frame, style="Data.TEntry", width=30)
+        self.set_entry_text(self.ent_name, self.participant_name)
+        self.ent_name.grid(row=0, column=1, sticky="sw", padx=(20,0))
 
 
         lbl_training_name = ttk.Label(self.active_frame, text="Maßnahmenbezeichnung", style="Data.TLabel")
         lbl_training_name.grid(row=1, column=0, sticky="e")
-        ent_t_name = ttk.Entry(self.active_frame, style="Data.TEntry", width=30)
-        self.set_entry_text(ent_t_name, "Individuelles Berufscoaching")
-        ent_t_name.grid(row=1, column=1, sticky="w", padx=(20,0))
+        self.ent_t_name = ttk.Entry(self.active_frame, style="Data.TEntry", width=30)
+        self.set_entry_text(self.ent_t_name, self.training_name)
+        self.ent_t_name.grid(row=1, column=1, sticky="w", padx=(20,0))
 
         lbl_training_nr = ttk.Label(self.active_frame, text="Maßnahmennummer", style="Data.TLabel")
         lbl_training_nr.grid(row=2, column=0, sticky="ne")
-        ent_t_nr = ttk.Entry(self.active_frame, style="Data.TEntry", width=30)
-        self.set_entry_text(ent_t_nr, "962/400/20")
-        ent_t_nr.grid(row=2, column=1, sticky="nw", padx=(20,0))
+        self.ent_t_nr = ttk.Entry(self.active_frame, style="Data.TEntry", width=30)
+        self.set_entry_text(self.ent_t_nr, self.training_nr)
+        self.ent_t_nr.grid(row=2, column=1, sticky="nw", padx=(20,0))
+
+        # back and forward button
+        self.create_button_back(row=3, collection_function=self.collection_data, nav_function=self.create_menu_functions)
+        self.create_button_forward(row=3, column=1, collection_function=self.collection_data, nav_function=self.create_menu_ze_coach)
+
+    def collection_data(self, event):
+        self.participant_name = self.ent_name.get()
+        self.training_name = self.ent_t_name.get()
+        self.training_nr = self.ent_t_nr.get()
+        print("-"*20)
+        print(self.participant_name)
+        print(self.training_nr)
+        print(self.training_name)
+        print("-"*20)
+
+    def collection_placeholder(self, event):
+        print("PLACEHOLDER")
+
+
+    def create_menu_ze_coach(self, event):
+
+        self.nav_frame.destroy()
+        self.active_frame.destroy()
+
+        format_choice = [0, 1, 0, 0, 0]
+        self.create_nav(format_choice)
+
+        self.active_frame = ttk.Frame(self, style="Intro.TFrame")
+        self.active_frame.grid(row=1, column=0, sticky="nsew")
+        self.active_frame.grid_columnconfigure(0, weight=1)
+        self.active_frame.grid_columnconfigure(1, weight=1)
+        for i in range(3):
+            self.active_frame.grid_rowconfigure(i, weight=1)
+        #
+        # lbl_name = ttk.Label(self.active_frame, text="Teilnehmer:in", style="Data.TLabel")
+        # lbl_name.grid(row=0, column=0, sticky="se")
+        # ent_name = ttk.Entry(self.active_frame, style="Data.TEntry", width=30)
+        # ent_name.grid(row=0, column=1, sticky="sw", padx=(20,0))
+        #
+        #
+        # lbl_training_name = ttk.Label(self.active_frame, text="Maßnahmenbezeichnung", style="Data.TLabel")
+        # lbl_training_name.grid(row=1, column=0, sticky="e")
+        # ent_t_name = ttk.Entry(self.active_frame, style="Data.TEntry", width=30)
+        # self.set_entry_text(ent_t_name, "Individuelles Berufscoaching")
+        # ent_t_name.grid(row=1, column=1, sticky="w", padx=(20,0))
+        #
+        # lbl_training_nr = ttk.Label(self.active_frame, text="Maßnahmennummer", style="Data.TLabel")
+        # lbl_training_nr.grid(row=2, column=0, sticky="ne")
+        # ent_t_nr = ttk.Entry(self.active_frame, style="Data.TEntry", width=30)
+        # self.set_entry_text(ent_t_nr, "962/400/20")
+        # ent_t_nr.grid(row=2, column=1, sticky="nw", padx=(20,0))
+
+        # back and forward button
+        self.create_button_back(row=3, collection_function=self.collection_placeholder,
+                                nav_function=self.create_menu_data)
+        self.create_button_forward(row=3, column=1, collection_function=self.collection_placeholder,
+                                   nav_function=self.create_menu_functions)
+
+    def create_button_back(self, row, collection_function, nav_function):
+        lbl_back = ttk.Label(self.active_frame, text="<<< Zurück", style="BackForw.TLabel")
+        lbl_back.grid(row=row, column=0, sticky="sw")
+        lbl_back.bind("<Button-1>", func=collection_function, add="+")
+        lbl_back.bind("<Button-1>", func=nav_function, add="+")
+
+    def create_button_forward(self, row, column, collection_function, nav_function):
+        lbl_forward = ttk.Label(self.active_frame, text="Vor >>>", style="BackForw.TLabel")
+        lbl_forward.grid(row=row, column=column, sticky="se")
+        lbl_forward.bind("<Button-1>", func=collection_function, add="+")
+        lbl_forward.bind("<Button-1>", func=nav_function, add="+")
 
     def create_nav(self, format_choice):
 
-
         styles = ["Inactive.Nav.TLabel", "Nav.TLabel", ]
-        print(styles[format_choice[0]])
         self.nav_frame = ttk.Frame(self, style="Nav.TFrame")
         self.nav_frame.grid(row=0, column=0, sticky="ew")
 
